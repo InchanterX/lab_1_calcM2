cfef177901e7d671ca7a67e8597aa8e3929a7bd2
InchanterX <inchanterx.work@gmail.com>
2025-10-03 23:18:04 +0300

    Added all the necessary tests

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,8 +1,9 @@
+import pytest
 from src.tokenizer import Tokenizer, Token
 
 def test_number_and_operations():
     t = Tokenizer()
-    expression="+3 + 4.25 * 2**3 // 5 + -3.00001"
+    expression="2+2*2"
     tokens = t.tokenize(expression)
 
     # Временный вывод
@@ -10,8 +11,31 @@ def test_number_and_operations():
     for i, token in enumerate(tokens):
         print(f"Токен {i}: {token.type} = {token.value} (позиция: {token.pos})")
 
-    assert [tok.type for tok in tokens] == ["OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "OPERATIONS", "NUMBER"]
+    assert [tok.type for tok in tokens] == ["NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER"]
 
+def test_brackets():
+    t = Tokenizer()
+    expression="(2+2)*2"
+    tokens = t.tokenize(expression)
+
+    # Временный вывод
+    print(f"\nТокенизация выражения: {expression!r}")
+    for i, token in enumerate(tokens):
+        print(f"Токен {i}: {token.type} = {token.value} (позиция: {token.pos})")
+
+    assert [tok.type for tok in tokens] == ["LBRACKET", "NUMBER", "OPERATIONS", "NUMBER", "RBRACKET", "OPERATIONS", "NUMBER"]
+
+def test_floats():
+    t = Tokenizer()
+    expression="(3.14+2)*2.0001"
+    tokens = t.tokenize(expression)
+
+    # Временный вывод
+    print(f"\nТокенизация выражения: {expression!r}")
+    for i, token in enumerate(tokens):
+        print(f"Токен {i}: {token.type} = {token.value} (позиция: {token.pos})")
+
+    assert [tok.type for tok in tokens] == ["LBRACKET", "NUMBER", "OPERATIONS", "NUMBER", "RBRACKET", "OPERATIONS", "NUMBER"]
 
 def test_extra_spaces():
     t = Tokenizer()
@@ -25,14 +49,20 @@ def test_extra_spaces():
 
     assert [tok.type for tok in tokens] == ["NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER", "OPERATIONS", "NUMBER"]
 
-# def test_unknown_input():
-#     t = Tokenizer()
-#     expression="3.1 + ананас"
-#     tokens = t.tokenize(expression)
+def test_unknown():
+    t = Tokenizer()
+    expression="2-a"
+    with pytest.raises(SyntaxError):
+        tokens = t.tokenize(expression)
 
-#     # Временный вывод
-#     print(f"\nТокенизация выражения: {expression!r}")
-#     for i, token in enumerate(tokens):
-#         print(f"Токен {i}: {token.type} = {token.value} (позиция: {token.pos})")
+def test_potentially_incorrect_expression():
+    t = Tokenizer()
+    expression="2*-2-"
+    tokens = t.tokenize(expression)
 
-#     assert [tok.type for tok in tokens] == ["NUMBER", "OPERATIONS", "UNKNOWN"]
+    # Временный вывод
+    print(f"\nТокенизация выражения: {expression!r}")
+    for i, token in enumerate(tokens):
+        print(f"Токен {i}: {token.type} = {token.value} (позиция: {token.pos})")
+
+    assert [tok.type for tok in tokens] == ["NUMBER", "OPERATIONS", "OPERATIONS", "NUMBER", "OPERATIONS"]
