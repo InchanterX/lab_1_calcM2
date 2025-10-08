b1441ea1d99c0bae71d5b7bab792f78c6a181bd1
InchanterX <inchanterx.work@gmail.com>
2025-10-08 16:16:52 +0300

    Added comments for Casino

--- src/casino.py
+++ src/casino.py
@@ -3,25 +3,32 @@ import src.constants
 
 
 class Casino:
+    '''
+    Give an ability to play casino in special mod of calculator.
+    It contains functions for variables, turning on and turning off,
+    promo codes, spins and payouts calculations.
+    '''
 
     def __init__(self):
         self.balance = 1000
+        # State of casino
         self.active = False
+        # State of promo code
         self.used_promo = False
 
     def enable(self):
-        # Активировать казино
+        # Enable casino
         self.active = True
         print("🎰 Казино включено! Добро пожаловать в MAI Casino!")
         print(f"Ваш баланс: {self.balance} MAI Coins")
 
     def disable(self):
-        # Выключение казино
+        # Disable casino
         self.active = False
         print("🎰 Казино выключено, но всегда можно сделать додеп!")
 
     def apply_promo(self, promo_code: str):
-        # Активировать промокод:
+        # Activate a promo if it is valid and haven't used yet
         if self.used_promo:
             print("Промокод уже был использован!")
         elif promo_code == "MAI_PO_BLATU":
@@ -32,7 +39,7 @@ class Casino:
             print("Неверный промокод!")
 
     def spin(self):
-        # Крутить рулетку!
+        # Spin the roulette for 100 coins!
         if self.balance < 100:
             print("Недостаточно средств для ставки (100 MAI Coins). Пополнить можно наличкой через автора кода, обмен монет 1 к 1.")
             return
@@ -44,6 +51,7 @@ class Casino:
         payout = self.calculate_payout(slots)
         self.balance += payout
 
+        # Messages for payouts
         if payout == 1500:
             print(f"!!!ДЖЭЭЭЭКПОООТ - {payout} MAI Coins!!!")
         elif payout > 0:
@@ -51,7 +59,7 @@ class Casino:
         print(f"Баланс: {self.balance} MAI Coins.")
 
     def calculate_payout(self, slots):
-        # Считает убытки казино
+        # Count payout
         if slots[0] == slots[1] == slots[2]:
             symbol = slots[0]
             return src.constants.MULTIPLIERS[symbol]*100
